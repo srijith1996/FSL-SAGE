12e60029b3f0e644197fcfabc52567135c74e062
Srijith Nair <nair.203@buckeyemail.osu.edu>
2025-01-15 21:11:24 -0500

    When cleaning saves dir also remove empty directories

--- saves/clean_saves_dir.py
+++ saves/clean_saves_dir.py
@@ -5,6 +5,25 @@
 import os, sys, shutil
 import export_runs_csv as erc
 
+def remove_empty_dirs(path):
+  'Function to remove empty folders'
+  if not os.path.isdir(path):
+    return
+
+  # remove empty subfolders
+  files = os.listdir(path)
+  if len(files):
+    for f in files:
+      fullpath = os.path.join(path, f)
+      if os.path.isdir(fullpath):
+        remove_empty_dirs(fullpath)
+
+  # if folder empty, delete it
+  files = os.listdir(path)
+  if len(files) == 0:
+    print("Removing empty folder: ", path)
+    os.rmdir(path)
+
 if __name__ == "__main__":
     print("NOTE THAT RUNNING THIS SCRIPT WHEN EXPERIMENTS ARE RUNNING, CAN LEAD TO UNWANTED DELETION OF ACTIVE DIRECTORIES.")
     choice = input("Are you sure you want to clean up the saves directory right now? [y/N]:  ").strip().lower() or 'n'
@@ -35,3 +54,5 @@ if __name__ == "__main__":
             shutil.rmtree(path) 
         elif not os.path.exists(path):
             print(f"Experiment dir {path} already removed")
+
+    remove_empty_dirs('.')
