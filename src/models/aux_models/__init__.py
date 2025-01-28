[12, Memory leak Fix] Use torch.no_grad() in the refreshdataset update() method to avoid an unnecessary gradient memory leak! Preallocate memory in the __init__() functions to avoid further memory leaks

src/models/aux_models/__init__.py
@@ -1,6 +1,6 @@
 # ------------------------------------------------------------------------------
 from abc import ABC, abstractmethod
-from tqdm import trange
+from tqdm import trange, tqdm
 import logging
 from typing import Dict, Type
 
@@ -41,7 +41,7 @@ class AuxiliaryModel(ABC, nn.Module):
 
         self.max_data_size = max_dataset_size
         self.refresh_dataset = None
-        self.align_dataloader = None
+        self.align_dataset = None
         self.align_batch_size = align_batch_size
         self.server = server
         self.aux_batch_handler_class = aux_batch_handler_class
@@ -68,15 +68,17 @@ class AuxiliaryModel(ABC, nn.Module):
         
         '''
         logging.debug(
-            f"Size of alignment dataset: {len(self.refresh_dataset)}"
+            f"Size of refresh dataset: {len(self.refresh_dataset)}"
         )
 
         data_y = None
-        
-        for i, data in enumerate(DataLoader(
+        refresh_dl = DataLoader(
             self.refresh_dataset, batch_size=self.align_batch_size,
             pin_memory=False, shuffle=False,
             collate_fn=self.refresh_dataset.collate_fn
+        )
+        for i, data in enumerate(tqdm(
+            refresh_dl, desc="Refresh Align Dataset", leave=False
         )):
             x, label = data[0], data[1]
             aux_ins = data[2:] if len(data) > 2 else []
@@ -90,11 +92,9 @@ class AuxiliaryModel(ABC, nn.Module):
             data_y = y_ if i == 0 else torch.cat((data_y, y_), axis=0)
             self.server.optimizer.zero_grad()
 
-        align_dataset = AlignDataset(self.refresh_dataset, data_y)
-        self.align_dataloader = DataLoader(
-            align_dataset, batch_size=self.align_batch_size,
-            shuffle=True, pin_memory=False, collate_fn=align_dataset.collate_fn
-        )
+        self.align_dataset = AlignDataset(self.refresh_dataset, data_y) \
+            if self.align_dataset is None else \
+                self.align_dataset.update_outs(data_y)
 
     #def get_align_dataset(self):
     #    '''Get X data concatenated with the labels'''
@@ -180,9 +180,14 @@ class GradScalarAuxiliaryModel(AuxiliaryModel):
 
     def align(self):
         bar = trange(self.align_epochs, desc="Alignment", leave=False)
-        logging.debug(f"# batches for alignment: {len(self.align_dataloader)}")
+        logging.debug(f"# samples for alignment: {len(self.align_dataset)}")
+        align_dataloader = DataLoader(
+            self.align_dataset, batch_size=self.align_batch_size,
+            shuffle=True, pin_memory=False,
+            collate_fn=self.align_dataset.collate_fn
+        )
         for i in bar:
-            for data in self.align_dataloader:
+            for data in align_dataloader:
                 x, y, labels = data[0], data[1], data[2]
                 other_ins = data[3:] if len(data) > 3 else []
                 assert x.shape == y.shape, \
@@ -196,7 +201,7 @@ class GradScalarAuxiliaryModel(AuxiliaryModel):
                     f" --- Epoch {i:4d}/{self.align_epochs}, Loss {loss:.4e}"
                 )
             bar.set_postfix(loss=loss.item())
-            if self.lr_scheduler is not None: self.lr_scheduler.step()
+            #if self.lr_scheduler is not None: self.lr_scheduler.step()
 
     def forward(self, x, label, *other_ins):
         x.requires_grad_(True)

src/models/aux_models/ds_utils.py
@@ -3,6 +3,7 @@ from abc import ABC, abstractmethod
 from typing import Type, Union
 import numpy as np
 import torch
+import logging
 from torch.utils.data import Dataset
 
 # ------------------------------------------------------------------------------
@@ -38,6 +39,10 @@ class AuxBatchHandler(ABC):
                 f"doesn't match the initialized type {self.aux_input_types[i]}"
 
     @abstractmethod
+    def __len__(self):
+        pass
+
+    @abstractmethod
     def __getitem__(self, idx):
         pass
 
@@ -46,62 +51,91 @@ class AuxBatchHandler(ABC):
         pass
 
     @abstractmethod
-    def maintain_size(self):
-        pass
-
-    @abstractmethod
     def collate(self, aux_inputs_list):
         pass
 
 # ------------------------------------------------------------------------------
 class RefreshDataset(Dataset):
+    inputs                  : torch.Tensor
+    labels                  : torch.Tensor
+    index                   : int
+    full                    : bool
+    max_data_size           : int
+    aux_batch_handler_class : Type[AuxBatchHandler]
+    aux_batch_handler       : AuxBatchHandler
+
     def __init__(self,
         inputs, labels, max_dataset_size, aux_inputs_list=[],
         aux_batch_handler_class: Type[AuxBatchHandler]=None
     ):
 
-        self.inputs = inputs
-        self.labels = labels
+        self.inputs = torch.empty((max_dataset_size, *inputs.shape[1:])
+                                  ).to(inputs.device)
+        self.labels = torch.empty((max_dataset_size, *labels.shape[1:])
+                                  , dtype=int).to(labels.device)
+        self.index = 0
+        self.full = False
+
         if len(aux_inputs_list) > 0:
             assert aux_batch_handler_class is not None, \
                 f"A RefreshDataset object requires an auxiliary batch handler" \
-                    + f"class if the aux_inputs_list is not empty"
+                    + f" class if the aux_inputs_list is not empty"
+            self.aux_batch_handler_class = aux_batch_handler_class
             self.aux_batch_handler = aux_batch_handler_class(
                 aux_inputs_list, max_dataset_size
             )
         else:
+            self.aux_batch_handler_class = None
             self.aux_batch_handler = None
-        self.max_data_size = max_dataset_size
-        self.__maintain_size()
 
-    def __maintain_size(self):
-        self.inputs = self.inputs[-self.max_data_size:]
-        self.labels = self.labels[-self.max_data_size:]
-        if self.aux_batch_handler is not None:
-            self.aux_batch_handler.maintain_size()
+        self.max_data_size = max_dataset_size
+
+        self.update(inputs, labels, aux_inputs_list)
 
     def __len__(self):
-        return len(self.inputs)
+        return self.max_data_size if self.full else self.index
 
     def __getitem__(self, idx):
+        if idx >= self.max_data_size:
+            raise IndexError(
+                f"Illegal index {idx} for dataset of size {self.max_data_size}"
+            )
+
         aux_inputs = self.aux_batch_handler[idx] \
             if self.aux_batch_handler is not None else []
         return self.inputs[idx], self.labels[idx], *aux_inputs
 
-    def update(self, inputs, labels, aux_inputs_list):
-        '''Append an existing batch of data (inputs, labels, auxiliary_inputs)
+    def update(self, inputs, labels, aux_inputs_list=[]):
+        with torch.no_grad():
+            batch_size = inputs.shape[0]
         
-        Note: To simplify things, we will next assume that if aux_inputs_list is
-        not empty it contains the auxiliary inputs of the gpt2 server modeis not
-        empty it contains the auxiliary inputs expected by the gpt2 server
-        model.'''
+            # Check if the new batch fits without exceeding the buffer
+            if self.index + batch_size <= self.max_data_size:
+                self.inputs[self.index:(self.index + batch_size)].copy_(inputs)
+                self.labels[self.index:(self.index + batch_size)].copy_(labels)
+                self.index = (self.index + batch_size) % self.max_data_size
+                # the modulo prevents the case where index = max_dataset_size
+                if self.index == 0: self.full = True
 
-        self.inputs = torch.cat((self.inputs, inputs), axis=0)
-        self.labels = torch.cat((self.labels, labels), axis=0)
-        if len(aux_inputs_list) > 0:
-            self.aux_batch_handler.verify_aux_inputs(aux_inputs_list)
-            self.aux_batch_handler.update(aux_inputs_list)
-        self.__maintain_size()
+            # if the current batch is smaller than the max size
+            elif batch_size <= self.max_data_size:
+                remaining_space = self.max_data_size - self.index
+                self.inputs[self.index:].copy_(inputs[:remaining_space])
+                self.inputs[:(batch_size - remaining_space)].copy_(inputs[remaining_space:])
+                self.labels[self.index:].copy_(labels[:remaining_space])
+                self.labels[:(batch_size - remaining_space)].copy_(labels[remaining_space:])
+                self.index = (self.index + batch_size) % self.max_data_size
+                self.full = True
+
+            # if the current batch is larger than the dataset size
+            else:
+                self.inputs.copy_(inputs[:self.max_data_size])
+                self.labels.copy_(inputs[:self.max_data_size])
+                self.index = 0
+                self.full = True
+
+            if len(aux_inputs_list) > 0:
+                self.aux_batch_handler.update(aux_inputs_list)
 
     def collate_fn(self, batch):
         inputs, labels, *aux_inputs_list = zip(*batch)
@@ -110,7 +144,7 @@ class RefreshDataset(Dataset):
         inputs = torch.stack(inputs)
         labels = torch.stack(labels)
 
-        proc_aux_ins = self.aux_batch_handler.collate(aux_inputs_list) \
+        proc_aux_ins = self.aux_batch_handler_class.collate(aux_inputs_list) \
             if len(aux_inputs_list) > 0 else []
 
         return (inputs, labels, *proc_aux_ins)
@@ -119,7 +153,8 @@ class RefreshDataset(Dataset):
 class AlignDataset(Dataset):
     def __init__(self, refresh_dataset: RefreshDataset, outputs: torch.Tensor):
         self.refresh_dataset = refresh_dataset
-        self.outputs = outputs
+        self.outputs = torch.empty_like(refresh_dataset.inputs)
+        self.outputs[:outputs.shape[0]].copy_(outputs)
 
     def __len__(self):
         return len(self.refresh_dataset)
@@ -128,6 +163,10 @@ class AlignDataset(Dataset):
         ref_dat = self.refresh_dataset[idx]
         aux_ins = ref_dat[2:] if len(ref_dat) > 2 else []
         return ref_dat[0], self.outputs[idx], ref_dat[1], *aux_ins
+
+    def update_outs(self, outs):
+        self.outputs[:outs.shape[0]].copy_(outs)
+        return self
 
     def collate_fn(self, batch):
         inputs, outputs, labels, *aux_inputs_list = zip(*batch)

src/models/aux_models/gpt2.py
@@ -1,4 +1,5 @@
 # ------------------------------------------------------------------------------
+from typing import Union, List
 import sys, logging
 import torch
 import torch.nn as nn
@@ -78,44 +79,123 @@ class GPT2LMAuxiliaryModel(aux_models.GradScalarAuxiliaryModel):
 
 # ------------------------------------------------------------------------------
 class GPT2AuxBatchHandler(AuxBatchHandler):
+    presents    : List[torch.Tensor]
+    input_shape : torch.Size
+    past        : List[Union[None, torch.Tensor]]
+    len_past    : Union[None, int]
+    size        : int
+    index       : int
+    max_size    : int
+
     def __init__(self, aux_inputs_list, max_size):
         super(GPT2AuxBatchHandler, self).__init__(aux_inputs_list, max_size)
 
         assert len(aux_inputs_list) == 4, \
             f"Expected 4 aux inputs for GPT2 model, got {len(aux_inputs_list)}"
-        self.presents, self.input_shape, self.past, self.len_past = \
-            tuple(aux_inputs_list)
 
-    def __update_kv_list(self, orig_list, list):
-        return [torch.cat((p, in_p), dim=1) for p, in_p in zip(orig_list, list)]
+        presents, _, past, _ = tuple(aux_inputs_list)
+        self.presents = [
+            torch.empty((p.shape[0], max_size, *p.shape[2:])
+                        ).to(presents[0].device)
+            for p in presents
+        ]
+        self.past = past if past[0] is None else [
+            torch.empty((p.shape[0], max_size, *p.shape[2:])
+                        ).to(past[0].device) for p in past
+        ]
 
-    def __update_presents(self, presents_batch):
-        '''Presents consists of a list of (key, value) tensors stacked along the
-        0th axis for each attention block.'''
-        self.presents = self.__update_kv_list(self.presents, presents_batch)
+        self.max_size = max_size
+        self.full = False
+        self.index = 0
+        self.len_past = aux_inputs_list[3]
+        self.update(aux_inputs_list)
 
-    def __update_input_shape(self, input_shape):
-        '''Input shape records the shape of the inputs that generated the
-        current set of outputs.  We need to simply increment the batch
-        dimension, dim=0, of our recorded input_shape with the size of the
-        incoming batch. '''
-        assert self.input_shape[1:] == input_shape[1:], \
-            f"Dimensions of input shape other than the batch dimension, " +\
-                f"dim=0, should match"
-        new_batch_size = self.input_shape[0] + input_shape[0]
-        self.input_shape = torch.Size([new_batch_size, *self.input_shape[1:]])
+    def __update_non_exhaustive(self,
+        batch_size, presents, input_shape, past, len_past
+    ):
+        for i, p in enumerate(presents):
+            self.presents[i][:, self.index:(self.index + batch_size), ...].copy_(p)
 
-    def __update_past(self, past):
-        assert isinstance(past, list)
-        if past[0] is not None and self.past[0] is not None:
-            self.past = self.__update_kv_list(self.past, past)
+        if self.past[0] is not None:
+            for i, p in enumerate(past):
+                self.past[i][:, self.index:(self.index + batch_size), ...].copy_(p)
+ 
+        self.input_shape = torch.Size(
+            [self.index + batch_size, *input_shape[1:]]
+        )
+        self.index = (self.index + batch_size) % self.max_size
+        if self.index == 0: self.full = True
 
-    def __update_len_past(self, len_past):
-        assert len_past == self.len_past, \
-            f"I don't know how to update a different value, {len_past}," + \
-                f"for len_past which is currently {self.len_past}"
+    def __update_exhaustive(self,
+        batch_size, presents, input_shape, past, len_past
+    ):
+        remaining_space = self.max_size - self.index
+
+        for i, p in enumerate(presents):
+            self.presents[i][:, self.index:, ...].copy_(p[:, :remaining_space, ...])
+            self.presents[i][:, :(batch_size-remaining_space), ...].copy_(
+                p[:, remaining_space:, ...]
+            )
+
+        if self.past[0] is not None:
+            for i, p in enumerate(past):
+                self.past[i][:, self.index:, ...].copy_(p[:, :remaining_space, ...])
+                self.past[i][:, :(batch_size-remaining_space), ...].copy_(
+                    p[:, remaining_space:, ...]
+                )
+
+        self.input_shape = torch.Size(
+            [self.max_size, *self.input_shape[1:]]
+        )
+        self.index = (self.index + batch_size) % self.max_size
+        self.full = True
+
+    def __update_bigger_batch(self,
+        presents, input_shape, past, len_past
+    ):
+        for i, p in enumerate(presents):
+            self.presents[i].copy_(p[:, :self.max_size, ...])
+
+        if self.past[0] is not None:
+            for i, p in enumerate(past):
+                self.past[i].copy_(p[:, :self.max_size, ...])
+
+        self.input_shape = torch.Size([
+            self.max_size, *list(input_shape[1:])
+        ])
+        self.index = 0
+        self.full = True
+
+    def update(self, aux_list):
+        presents, input_shape, past, len_past = tuple(aux_list)
+        batch_size = presents[0].shape[1]
+
+        if self.index + batch_size <= self.max_size:
+            self.__update_non_exhaustive(
+                batch_size, presents, input_shape, past, len_past
+            )
+
+        # if the current batch is smaller than the max size
+        elif batch_size <= self.max_size:
+            self.__update_exhaustive(
+                batch_size, presents, input_shape, past, len_past
+            )
+
+        # if the current batch is larger than the dataset size
+        else:
+            self.__update_bigger_batch(
+                presents, input_shape, past, len_past
+            )
+
+    def __len__(self):
+        return self.max_size if self.full else self.index
 
     def __getitem__(self, idx):
+        if idx > self.__len__():
+            raise IndexError(
+                f"Illegal index {idx} for dataset of size {self.__len__()}"
+            )
+
         pres = [p[:, idx, ...] for p in self.presents]
         in_shp = torch.Size([1, *self.input_shape[1:]])
         past = self.past if self.past[0] is None else \
@@ -123,35 +203,8 @@ class GPT2AuxBatchHandler(AuxBatchHandler):
         len_past = self.len_past
         return pres, in_shp, past, len_past
 
-    def update(self, aux_list):
-        present, input_shape, past, len_past = tuple(aux_list)
-        self.__update_presents(present)
-        self.__update_input_shape(input_shape)
-        self.__update_past(past)
-        self.__update_len_past(len_past)
-
-    def maintain_size(self):
-        if self.presents[0].shape[1] <= self.max_size:
-            #logging.info(f"Skipping, current presents len {self.presents[0].shape[1]}")
-            return # no need to truncate
-
-        #logging.info("About to truncate dataset.  Init sizes----------")
-        #logging.info(f"Presents: {self.presents[0].shape[1]}")
-        #logging.info(f"Input shape: {self.input_shape}")
-        #logging.info(f"Past: {self.past}")
-        #logging.info(f"Past len: {self.len_past}")
-
-        self.presents = [p[:, -self.max_size:, ...] for p in self.presents]
-        new_batch_size = min(self.max_size, self.input_shape[0])
-        self.input_shape = torch.Size([new_batch_size, *self.input_shape[1:]])
-        if self.past[0] is not None:
-            self.past = [p[:, -self.max_size:, ...] for p in self.past]
-
-        #logging.info("Maintained sizes at ----------")
-        #logging.info(f"Presents: {self.presents[0].shape[1]}")
-        #logging.info(f"Input shape: {self.input_shape}")
-
-    def collate(self, aux_inputs_list):
+    @staticmethod
+    def collate(aux_inputs_list):
         presents_list, input_shape_list, past_list, len_past_list = \
             tuple(aux_inputs_list)
 